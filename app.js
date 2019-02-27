2b207e38fbec65d19bbb3a7578714802e253a748
Jonh Silva <jonhsilvaprojetos@gmail.com>
2019-02-27 00:16:03 -0300

    Subindo os primeiros arquivos e modulos

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+
+// Configurações
+
+    // Sessão
+    app.use(session({
+        secret: "easyfrontapp",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+    // Bodyparse
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+
+    // Handlebars Configurando engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+
+
+    // Configurando diretorio estático
+    app.use(express.static(path.join(__dirname,"public")))
+
+    // Rotas
+
+    // Rota default
+    app.get('/', (req, res) => {
+        res.render('home');
+    })
+
+    // Rota admin
+    
+
+    // Iniciando servidor
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log('Servidor Rodando!')
+    })
